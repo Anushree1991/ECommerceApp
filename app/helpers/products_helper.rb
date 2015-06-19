386a2b7059d1f3d8fb26049ca08c2be035bf6164
amruthesh <amruthesh.g@jifflenow.com>
2015-06-19 20:32:34 +0530

    Amruthesh: product category

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,5 @@
 module ProductsHelper
+  def categories_select
+    Category.all.collect{|category| [category.name, category.id]}
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150619130051) do
+ActiveRecord::Schema.define(version: 20150619144255) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "categories", force: true do |t|
     t.string   "name"
@@ -22,7 +25,7 @@ ActiveRecord::Schema.define(version: 20150619130051) do
   create_table "products", force: true do |t|
     t.string   "name"
     t.string   "image"
-    t.float    "price",       limit: 24
+    t.float    "price"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "category_id"
